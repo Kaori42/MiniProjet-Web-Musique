e88d5a006f5c5cc2cf4737d6830ae65873102802
Matthieu Blackowiak <93971368+Kaori42@users.noreply.github.com>
2022-11-22 23:32:48 +0100

    V2
    Optimisation du code, principalement cache sélecteur Jquery et fonction

--- scripts/Player.js
+++ scripts/Player.js
@@ -8,12 +8,13 @@ function openSong($musique){
     setTimeout(function (){
         timer();
     }, 10)
-    $('#player').on("timeupdate", Update);
+    $("#player").on("timeupdate", Update);
 }
 
 function changeSong($musique){
-    if($musique!==($('#player').attr("src"))){
-        $('#player').attr("src", $musique);
+    let player = $("#player");
+    if($musique!==(player.attr("src"))){
+        player.attr("src", $musique);
     }
 }
 
@@ -24,43 +25,52 @@ function stop(){
 }
 
 function change_vol() {
-    $('#player').prop("volume",$('#change_vol').val());
+    $("#player").prop("volume",$('#change_vol').val());
 }
 
 function play(){
-    $('#player').trigger("play");
+    $("#player").trigger("play");
     $('#playerPlay').css('display','none');
     $('#playerPause').css('display','inline');
 }
 
 function pause(){
-    $('#player').trigger("pause");
+    $("#player").trigger("pause");
     $('#playerPlay').css('display','inline');
     $('#playerPause').css('display','none');
 }
 
 function mute_vol(){
-    $("#player").prop("muted",!$("#player").prop("muted"));
+    let player = $("#player");
+    player.prop("muted",!player.prop("muted"));
 }
 
 function change_time(){
     $('#player').prop("currentTime",$('#timeline').val());
 }
 
+function temps(prop){
+    let player = $("#player");
+    let min = Math.floor(player.prop(prop)/60);
+    let sec = Math.floor(((player.prop(prop)/60)-min)*60);
+    if (sec<10) sec="0"+sec;
+    if (sec===60) {sec="00"; min++;}
+    return {
+        'min': min,
+        'sec': sec
+    };
+}
+
 function timer(){
-    $('#timeline').attr("max", $('#player').prop("duration"));
-    let minT = Math.floor($('#player').prop("duration")/60);
-    let secT = Math.floor((($('#player').prop("duration")/60)-minT)*60);
-    if (secT<10) secT="0"+secT;
-    if (secT===60) {secT="00"; minT++;}
-    $('#timeTotal').text(minT+":"+secT);
+    let player = $("#player");
+    $('#timeline').attr("max", player.prop("duration"));
+    let {min, sec} = temps("duration");
+    $('#timeTotal').text(min+":"+sec);
 }
 
 function Update(){
-    $('#timeline').val(($('#player').prop("currentTime")));
-    let min = Math.floor($('#player').prop("currentTime")/60);
-    let sec = Math.floor((($('#player').prop("currentTime")/60)-min)*60);
-    if (sec<10) sec="0"+sec;
-    if (sec===60) {sec="00"; min++;}
+    let player = $("#player");
+    $('#timeline').val((player.prop("currentTime")));
+    let {min, sec} = temps("currentTime");
     $('#time').text(min+":"+sec);
 }
